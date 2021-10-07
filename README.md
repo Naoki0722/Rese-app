90aa35ff48e36c02fe21a20a0249b585f6121e81
kadukimochida <dolce.km.3a@gmail.com>
2021-10-07 15:31:11 +0900

    read me

--- README.md
+++ README.md
@@ -1,18 +1,54 @@
 # Rese
 
-Restaurant reservation app
+Restaurant reservation service of corporate group companies
+
+# Overview
+
+* Simple functions and screens are easy to understand and use
 
 # DEMO
 
 what demo?
 
-# Features
-
-Simple functions and screens are easy to understand and use
-
 # Requirement
+
+PHP 8.0.9
+
+Composer version 2.1.3
 
 Laravel Framework 8.61.0
 
-PHP 8.0.9 
+# Installation
 
+MAMP install 
+
+[Mac](https://www.mamp.info/en/downloads/)
+
+XAMPP install
+
+[Windows](https://www.apachefriends.org/jp/index.html)
+
+## Composer install
+
+[Mac](https://getcomposer.org/download/)
+
+Click the link for the latest version of 2.〇.〇 from Manual Download at the bottom of the page
+
+Go to the Download directory
+
+```
+$ sudo mv composer.phar /usr/local/bin/composer
+```
+```
+$ chmod a+x /usr/local/bin/composer
+```
+
+[Windows](https://getcomposer.org/doc/00-intro.md#installation-windows)
+
+Download the installer from the link "Composer-Setup.exe" in the text "Installation – Using the Installer for Windows".
+
+# Author
+
+* Kazuki Mochida
+* COACHTECH
+* kaduki.mochida@gmail.com
--- resources/views/auth/mypage.blade.php
+++ resources/views/auth/mypage.blade.php
@@ -7,6 +7,7 @@
     <div class="flex justify-around w-11/12 m-auto">
       <div class="w-4/12">
         <h2>予約状況</h2>
+
         @if(@isset($reservedShops))
         @foreach($reservedShops as $reservedShop)
         
@@ -14,11 +15,11 @@
           <div class="flex justify-between ">
             <div class="flex">
               <img src="../img/clock.jpeg" class="w-10 h-10">
-              <p>予約</p>
+              <p>予約{{$loop->iteration}}</p>
             </div>
             <form action="/reserved/delete/{{$reservedShop->id}}" method="post">
             @csrf
-              <button class="">&otimes;</button>
+              <button class="text-white">&otimes;</button>
             </form>
           </div>
           <div class="my-2.5">
@@ -46,11 +47,26 @@
         <h2>{{$user->name}}さん</h2>
         @endif
         <p>お気に入り店舗</p>
-        <div class="flex flex-wrap">
+        <div class="flex flex-wrap justify-around">
           @if(@isset($favorites))
           @foreach($favorites as $favorite)
-          <div>
-            <p>{{$favorite}}</p>
+          <div class="w-2/5 h-64 m-3 bg-white rounded-md shadow-kk">
+            <img src="{{$favorite->shop->img}}" class="w-full h-3/5 rounded-t-md">
+            <div class="p-2.5">
+              <p class="font-semibold">{{$favorite->shop->shop_name}}</p>
+              <p class="inline-block text-xs">#{{$favorite->shop->area->area}}</p>
+              <p class="inline-block text-xs">#{{$favorite->shop->category->category}}</p>
+              <div class="flex justify-between">
+                <form action="datail/:{{$favorite->shop->id}}" method="get" class="inline-block">
+                  @csrf
+                  <button class="text-xs text-white font-medium rounded-md bg-blue-600 w-24 h-6">詳しくみる</button>
+                </form>
+                <form action="/favorite/delete/{{$favorite->id}}" method="post">
+                @csrf
+                  <button class="text-red-400 text-3xl">&#9829;</button>
+                </form>
+              </div>
+            </div>
           </div>
           @endforeach
           @endif
@@ -65,7 +81,6 @@
     data: {
     isOpen: false,
     reserveCount:1,
-    
     },
   })
 </script>
