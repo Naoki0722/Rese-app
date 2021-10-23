13976943e252f9ff98a512a0a1f7983f8f5e3451
kadukimochida <dolce.km.3a@gmail.com>
2021-10-23 16:51:08 +0900

    evaluaion.bladeのheader

--- resources/views/auth/evaluation.blade.php
+++ resources/views/auth/evaluation.blade.php
@@ -2,14 +2,19 @@
 
 @section('content')
 <div id="app" class="font-sans text-gray-900 antialiased h-screen w-screen  bg-gray-100">
-  <div v-if="user.role === 'customer' || user.role === ''">
-    <x-after-header></x-after-header>
+  <div v-if="user">
+    <div v-if="user.role === 'customer' || user.role === ''">
+      <x-after-header></x-after-header>
+    </div>
+    <div v-else-if="user.role === 'admin'">
+      <x-admin-header></x-admin-header>
+    </div>
+    <div v-else-if="user.role === 'owner'">
+      <x-owner-header></x-owner-header>
+    </div>
   </div>
-  <div v-else-if="user.role === 'admin'">
-    <x-admin-header></x-admin-header>
-  </div>
-  <div v-else-if="user.role === 'owner'">
-    <x-owner-header></x-owner-header>
+  <div v-else>
+    <x-header></x-header>
   </div>
   <div class="bg-gray-100 h-screen" :class="isOpen ? 'hidden' : 'block' ">
     <div class="m-auto w-11/12 h-3/4 flex justify-around md:block md:w-full">
